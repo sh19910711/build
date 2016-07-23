2b948f4e14b7a600180b7d600cfceca056635232
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-23 04:22:20 +0000

    wip

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import log "github.com/Sirupsen/logrus"
 import "github.com/gin-gonic/gin"
+import "net/http"
 
 func main() {
 	log.Info("starting build server")
 
 	r := gin.Default()
+
+	r.GET("/", func (c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"msg": "hello"})
+	})
 
 	r.Run()
 }
