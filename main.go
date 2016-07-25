255e1cf5b0fc6045ec6ba477b53315d56707923b
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-25 12:57:15 +0000

    wip

--- main.go
+++ main.go
@@ -9,16 +9,92 @@ import "io/ioutil"
 import "os"
 import "archive/tar"
 import "bytes"
+import "path"
 import "github.com/docker/engine-api/client"
 import "github.com/docker/engine-api/types"
 import "github.com/docker/engine-api/types/container"
 import "golang.org/x/net/context"
 
+const DOCKER_ENDPOINT = "unix:///var/run/docker.sock"
+const DOCKER_VERSION = "v1.18"
+
+func newWorker() (w worker, err error) {
+	headers := map[string]string{
+		"User-Agent": "engine-api",
+	}
+	w.c, err = client.NewClient(DOCKER_ENDPOINT, DOCKER_VERSION, nil, headers)
+	return w, err
+}
+
+type worker struct {
+	c  *client.Client
+	id string
+}
+
+func (w *worker) Create() (err error) {
+	config := container.Config{
+		Image: "build",
+		Cmd:   []string{"bash", "/build.bash"},
+	}
+
+	c, err := w.c.ContainerCreate(context.Background(), &config, nil, nil, "")
+	if err != nil {
+		return err
+	}
+
+	w.id = c.ID
+	log.Info(w.id, ": created")
+	return nil
+}
+
+func archive(src string) (*bytes.Reader, error) {
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+
+	content, err := ioutil.ReadFile(src)
+	if err != nil {
+		return nil, err
+	}
+
+	h := &tar.Header{
+		Name: path.Base(src),
+		Mode: 0755,
+		Size: int64(len(content)),
+	}
+	if err := tw.WriteHeader(h); err != nil {
+		return nil, err
+	}
+	if _, err := tw.Write(content); err != nil {
+		return nil, err
+	}
+	if err := tw.Close(); err != nil {
+		return nil, err
+	}
+
+	return bytes.NewReader(buf.Bytes()), nil
+}
+
+func (w *worker) Copy(src string, dst string) error {
+	r, err := archive(src)
+	if err != nil {
+		return err
+	}
+	if err := w.c.CopyToContainer(context.Background(), w.id, "/", r, types.CopyToContainerOptions{}); err != nil {
+		return err
+	}
+	log.Info(w.id, ": copy ", src, " => ", dst)
+	return nil
+}
+
+func (w *worker) Start() error {
+	log.Info(w.id, ": start")
+	return w.c.ContainerStart(context.Background(), w.id, types.ContainerStartOptions{})
+}
+
 func main() {
 	log.Info("starting build server")
 
-	defaultHeaders := map[string]string{"User-Agent": "engine-api"}
-	client, err := client.NewClient("unix:///var/run/docker.sock", "v1.18", nil, defaultHeaders)
+	w, err := newWorker()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,58 +105,18 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"msg": "hello"})
 	})
 
-	r.GET("/docker/containers", func(c *gin.Context) {
-		options := types.ContainerListOptions{All: true}
-		containers, err := client.ContainerList(context.Background(), options)
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.JSON(http.StatusOK, gin.H{"containers": containers})
-	})
-
-	// exec test
 	r.GET("/docker/exec", func(c *gin.Context) {
-		// create container
-		config := container.Config{
-			Image: "curl",
-			Cmd:   []string{"bash", "/build.bash"},
-			// AttachStdin: true,
-			// Tty: true,
-		}
-		worker, err := client.ContainerCreate(context.Background(), &config, nil, nil, "")
-		if err != nil {
+		if err := w.Create(); err != nil {
 			log.Fatal(err)
 		}
-		log.Info("created: ", worker.ID)
 
-		// open
-		f, err := os.Open("./script/build.tar")
-		if err != nil {
+		if err := w.Copy("./script/build.bash", "/"); err != nil {
 			log.Fatal(err)
 		}
-		defer f.Close()
 
-		// copy script
-		err = client.CopyToContainer(context.Background(), worker.ID, "/", f, types.CopyToContainerOptions{})
-		if err != nil {
+		if err := w.Start(); err != nil {
 			log.Fatal(err)
 		}
-		log.Info("copied: build.bash")
-
-		// start container
-		err = client.ContainerStart(context.Background(), worker.ID, types.ContainerStartOptions{})
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Info("started: ", worker.ID)
-
-		// stop
-		// t := 0*time.Second
-		// err = client.ContainerStop(context.Background(), worker.ID, &t)
-		// if err != nil {
-		//   log.Fatal(err)
-		// }
-		// log.Info("stopped: ", worker.ID)
 	})
 
 	r.POST("/tar", func(c *gin.Context) {
