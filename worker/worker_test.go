ad39ecbcd9e59828799732745e657dbf09d16c9f
Hiroyuki Sano <sh19910711@gmail.com>
2016-08-01 23:11:32 +0000

    worker: add test for Create

---
+++ worker/worker_test.go
@@ -0,0 +1,15 @@
+package worker_test
+
+import (
+	"github.com/codestand/build/worker"
+	"golang.org/x/net/context"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	ctx := context.Background()
+	w, _ := worker.New()
+	if err := w.Create(ctx, "build", "bash /build.bash"); err != nil {
+		t.Fatal(err)
+	}
+}
