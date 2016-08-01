210b05d667826e69b371b91856360804f4117cbe
Hiroyuki Sano <sh19910711@gmail.com>
2016-08-01 03:37:11 +0000

    worker: mkdir before copy from container

--- main.go
+++ main.go
@@ -92,6 +92,10 @@ func (w *worker) CopyFile(src string, dst string) error {
 }
 
 func (w *worker) CopyFromWorker(src, dstPrefix string) error {
+	// mkdir dstPrefix
+	if err := os.MkdirAll(dstPrefix, 0755); err != nil {
+		return err
+	}
 	// get file from worker (as a tar-ball archive)
 	r, _, err := w.c.CopyFromContainer(w.ctx, w.id, src)
 	if err != nil {
