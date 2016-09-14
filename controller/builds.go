13f3be58ce53046d002e87b5fa7e12d1b898d4a2
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-14 01:46:02 +0000

    builds: add Build

--- controller/builds.go
+++ controller/builds.go
@@ -7,36 +7,67 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 )
+
+type Build struct {
+	job jobqueue.Job
+}
+
+func (b *Build) saveSource(c *gin.Context, prefix string) error {
+	if err := os.MkdirAll(prefix, 0700); err != nil {
+		return err
+	}
+
+	r, err := formFileReader(c)
+	if err != nil {
+		return err
+	}
+	src := filepath.Join(prefix, b.job.Id)
+	if err := save(r, src); err != nil {
+		return err
+	} else {
+		b.job.Src = src
+	}
+
+	return nil
+}
+
+func (b *Build) setup(c *gin.Context) {
+	b.job.Callback = c.PostForm("callback")
+	b.job.Image = "build"
+	b.job.Commands = []string{"make"}
+	b.job.Artifacts = []string{"/app/app"}
+}
 
 // POST /builds
 // - params[file] := tar-ball (required)
 // - params[callback] := URL fired after completed build (required)
 // - returns {"id": "<job-id>"}
 func Create(c *gin.Context) {
-	log.Debug("1. create new job")
-	job := jobqueue.NewJob()
+	b := newBuild()
 
-	log.Debug("2. save source to tmp")
-	r := formfile(c)
-	if err := os.MkdirAll("./tmp", 0700); err != nil {
-		log.Fatal(err)
-	}
-	src := "./tmp/" + job.Id
-	if err := save(r, src); err != nil {
-		log.Fatal(err)
+	log.Debug("Create: save source to tmp")
+	if err := b.saveSource(c, "./tmp"); err != nil {
+		respondError(c, err)
+		return
 	}
 
-	log.Debug("3. push job to jobqueue")
-	job.Src = src
-	job.Callback = c.PostForm("callback")
-	job.Image = "build"
-	job.Commands = []string{"make"}
-	job.Artifacts = []string{"/app/app"}
-	jobqueue.Push(job)
+	b.setup(c)
 
-	log.Debug("4. return job id")
-	c.JSON(http.StatusOK, gin.H{"id": job.Id})
+	log.Debug("Create: push build job")
+	go jobqueue.Push(b.job)
+
+	log.Debug("Create: success")
+	c.JSON(http.StatusOK, gin.H{"id": b.job.Id})
+}
+
+func respondError(c *gin.Context, err error) {
+	c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+}
+
+func newBuild() Build {
+	return Build{job: jobqueue.NewJob()}
 }
 
 func save(r io.Reader, path string) error {
@@ -50,10 +81,10 @@ func save(r io.Reader, path string) error {
 	return nil
 }
 
-func formfile(c *gin.Context) io.Reader {
-	r, _, err := c.Request.FormFile("file")
-	if err != nil {
-		log.Fatal("formfile: ", err)
+func formFileReader(c *gin.Context) (io.Reader, error) {
+	if r, _, err := c.Request.FormFile("file"); err != nil {
+		return nil, err
+	} else {
+		return r, nil
 	}
-	return r
 }
