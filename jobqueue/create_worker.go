633f055c2b4cffd52e38863d64840e73a2a39936
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-21 04:04:14 +0000

    jobqueue: use Dockerfile if exists

--- jobqueue/create_worker.go
+++ jobqueue/create_worker.go
@@ -3,6 +3,7 @@ package jobqueue
 import (
 	"github.com/codestand/build/util"
 	"github.com/codestand/build/worker"
+	uuid "github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"os"
 )
@@ -10,7 +11,11 @@ import (
 func CreateWorker(src string) (w worker.Worker, err error) {
 	w = worker.New()
 
-	if err := w.Create(context.Background(), "build", "bash /build.bash"); err != nil {
+	if err := buildWithDockerfileIfExists(w, src); err != nil {
+		return w, err
+	}
+
+	if err := w.Create(context.Background(), w.ImageName, "bash /build.bash"); err != nil {
 		return w, err
 	}
 
@@ -23,6 +28,28 @@ func CreateWorker(src string) (w worker.Worker, err error) {
 	}
 
 	return w, nil
+}
+
+func buildWithDockerfileIfExists(w worker.Worker, tarPath string) error {
+	tarball, err := os.Open(tarPath)
+	if err != nil {
+		return err
+	}
+	if ok, err := util.CheckFileInTar(tarball, "Dockerfile"); err != nil {
+		return err
+	} else if ok {
+		if r, err := util.ReadFileFromTar(tarball, "Dockerfile"); err != nil {
+			return err
+		} else {
+			imageName := uuid.NewV4().String()
+			if err := w.ImageBuild(context.Background(), imageName, r); err != nil {
+				return err
+			} else {
+				w.ImageName = imageName
+			}
+		}
+	}
+	return nil
 }
 
 func copyAppCodeToWorker(w worker.Worker, tarPath string) error {
