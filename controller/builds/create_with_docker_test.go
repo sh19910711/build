b128bf24cedf2a96e01edd047b49de8dfb6c8fd6
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-21 11:41:15 +0000

    builds: test build log

--- controller/builds/create_with_docker_test.go
+++ controller/builds/create_with_docker_test.go
@@ -8,7 +8,10 @@ import (
 	"github.com/codestand/build/test/testhelper"
 	"github.com/codestand/build/test/testhelper/controller_helper"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 )
@@ -72,5 +75,22 @@ func TestCreate(t *testing.T) {
 		}
 	case <-time.After(3 * time.Second):
 		t.Fatal("the build should be finished in a few second")
+	}
+
+	req, err := testhelper.Get(s.URL+"/builds/"+build.Id+"/log.txt", map[string]string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if b, err := ioutil.ReadAll(res.Body); err != nil {
+		t.Fatal(err)
+	} else if !strings.Contains(string(b), "make") {
+		t.Fatal("example build shuold start with make command")
 	}
 }
