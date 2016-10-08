17a0b4d1e7e684c882375f290dde1640e738457b
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-08 02:56:38 +0000

    wiip

--- worker/image_build_with_docker_test.go
+++ worker/image_build_with_docker_test.go
@@ -24,10 +24,12 @@ func TestImageBuild(t *testing.T) {
 	t.Run("without cmd", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		defer cancel()
+
 		w := worker.New()
 		w.Image = "cs-build/test/hello"
-		buf := bytes.NewBufferString(DOCKERFILE_WITHOUT_CMD)
-		if err := w.ImageBuild(ctx, buf); err != nil {
+
+		r := bytes.NewBufferString(DOCKERFILE_WITHOUT_CMD)
+		if err := w.ImageBuild(ctx, r); err != nil {
 			t.Fatal(err)
 		}
 	})
@@ -35,10 +37,12 @@ func TestImageBuild(t *testing.T) {
 	t.Run("failed", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		defer cancel()
+
 		w := worker.New()
 		w.Image = "cs-build/test/fail"
-		buf := bytes.NewBufferString(DOCKERFILE_FAILED)
-		if err := w.ImageBuild(ctx, buf); err == nil {
+
+		r := bytes.NewBufferString(DOCKERFILE_FAILED)
+		if err := w.ImageBuild(ctx, r); err == nil {
 			t.Fatal("build should be failed")
 		}
 	})
