3e1c72b12c8c06ddfdc0aa3a087b2958c8c8e650
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-17 21:31:18 +0000

    edit main.go

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"github.com/codestand/build/controller/builds"
+	"github.com/codestand/build/controller"
 	_ "github.com/codestand/build/env"
 	"github.com/codestand/build/jobqueue"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	go jobqueue.Wait()
 	defer jobqueue.Close()
-
-	r := gin.Default()
-	builds.Mount(r)
-	r.Run()
+	controller.Mount()
 }
