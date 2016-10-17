dd5981c54fd916f3bdd95cbc277629a29d07641d
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-17 22:12:12 +0000

    edit main.go

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"github.com/codestand/build/controller"
 	_ "github.com/codestand/build/env"
-	"github.com/codestand/build/jobqueue"
 )
 
 func main() {
-	go jobqueue.Wait()
-	defer jobqueue.Close()
 	controller.Mount()
 }
