fa2de6cdb1e692b5959771bb81f642475a9db462
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-17 21:30:52 +0000

    release: show log before start

--- env/release.go
+++ env/release.go
@@ -3,11 +3,14 @@
 package env
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
 )
 
 const DEBUG = false
 
 func init() {
+	log.SetLevel(log.DebugLevel)
+	log.Info("build server started in the release mode")
 	gin.SetMode(gin.ReleaseMode)
 }
