37c52b44708fb8ac23f074df1d523bed4f3773cf
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-18 19:50:38 +0000

    archive: add GetFileReaderFromTar()

--- archive/tar.go
+++ archive/tar.go
@@ -3,6 +3,7 @@ package archive
 import (
 	"archive/tar"
 	"bytes"
+	"io"
 	"io/ioutil"
 	"path"
 )
@@ -14,7 +15,32 @@ type Tar struct {
 	Size    int64
 }
 
-// TODO: options for tar header
+// TODO: file path?
+func GetFileReaderFromTar(tarball io.Reader, filename string) (nilReader io.Reader, err error) {
+	tr := tar.NewReader(tarball)
+
+	for { // each file
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nilReader, err
+		}
+
+		if header.Name != filename {
+			continue
+		}
+
+		buf := &bytes.Buffer{}
+		if _, err := io.CopyN(buf, tr, header.Size); err != nil {
+			return nilReader, err
+		}
+		return bytes.NewReader(buf.Bytes()), nil
+	}
+
+	return nilReader, nil
+}
+
 func TarFromFile(filepath string) (*Tar, error) {
 	if content, err := ioutil.ReadFile(filepath); err != nil {
 		return nil, err
@@ -30,6 +56,7 @@ func TarFromBuffer(b *bytes.Buffer, filename string) *Tar {
 func (t *Tar) Reader() (*bytes.Reader, error) {
 	buf := new(bytes.Buffer)
 	tw := tar.NewWriter(buf)
+	defer tw.Close()
 
 	if err := tw.WriteHeader(&tar.Header{Name: t.Name, Mode: t.Mode, Size: t.Size}); err != nil {
 		return nil, err
