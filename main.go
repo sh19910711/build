e28c352f3b27856aec768e36aacae747d840a072
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-20 01:00:29 +0000

    edit main.go

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"github.com/codestand/build/controller"
 	_ "github.com/codestand/build/env"
+	"github.com/codestand/build/job"
+	"github.com/codestand/build/model"
 )
 
 func main() {
+	model.Open()
+	defer model.Close()
+	go job.Wait()
 	controller.Mount()
 }
